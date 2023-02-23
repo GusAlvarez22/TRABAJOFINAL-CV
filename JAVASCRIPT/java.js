fd7e7cc43e59009731140af34520bfafabe08e8e
GusAlvarez22 <gustavojalvarez16@gmail.com>
2023-02-23 20:30:02 -0300

    ya lo tengo

---
+++ JAVASCRIPT/java.js
@@ -0,0 +1,8 @@
+document.getElementById("botonocultar").addEventListener("click", function(){
+    document.getElementById("datospersonales").style.display="none"
+    
+    })
+    document.getElementById("botonomostrar").addEventListener("click", function(){
+        document.getElementById("datospersonales").style.display="block";      
+})
+
