8b26334652b669dfa256f8b7a190ee905d2f38b8
GusAlvarez22 <gustavojalvarez16@gmail.com>
2023-02-24 12:06:28 -0300

    casi listo

--- JAVASCRIPT/java.js
+++ JAVASCRIPT/java.js
@@ -6,3 +6,19 @@ document.getElementById("botonocultar").addEventListener("click", function(){
         document.getElementById("datospersonales").style.display="block";      
 })
 
+
+document.getElementById("botonocultar2").addEventListener("click", function(){
+    document.getElementById("explab").style.display="none"
+    
+    })
+    document.getElementById("botonomostrar2").addEventListener("click", function(){
+        document.getElementById("explab").style.display="block";      
+})
+
+document.getElementById("botonocultar3").addEventListener("click", function(){
+    document.getElementById("ACADEMICA").style.display="none"
+    
+    })
+    document.getElementById("botonomostrar3").addEventListener("click", function(){
+        document.getElementById("ACADEMICA").style.display="block";      
+})
